22e78a3bb5d165354aee436f63abd6a7c5764778
vlad <vskiev@gmail.com>
2017-10-28 16:36:04 +0300

    shwalengthimeter

--- multiplaction.py
+++ multiplaction.py
@@ -4,12 +4,8 @@
 #     for j in range(1,11):
 #         print("%4d" % (i*j), end='')
 #     print()
-
 #v2
 for i in range(1,11):
     for j in range(1,11):
         print('{:>4}'.format((i*j)), end='')
     print()
-
-
-
