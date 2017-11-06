36744a238003a614a89af99bb4c9e4c2ee173ffb
vlad <vskiev@gmail.com>
2017-11-06 17:18:51 +0200

    bat

---
+++ battaryCpacity.py
@@ -0,0 +1,7 @@
+#Task is to return the value of left battery capacity
+# parsed from given string (let's assume it is some shell command's result).
+#Current level should be calculated as CurrentCapacity / MaxCapacity
+# in percents. The resulting value should be like the following example:
+#61.41%
+
+
